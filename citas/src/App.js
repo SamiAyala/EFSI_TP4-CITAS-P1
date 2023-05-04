76df82448ea4f9b584c0cbf3bdd819c34d75df49
SamiAyala <samirayala23@gmail.com>
2023-05-04 10:38:49 -0300

    Creacion de proyecto

---
+++ citas/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1 className="titulo">Administrador de Pacientes</h1>
+       <div className="container">
+        <div className="row">
+          <div className="one-half-column">
+            <h2>Crear Mi Cita</h2>
+          </div>
+          <div className="one-half-column">
+            <h2>Administra tus Citas</h2>
+          </div>
+        </div>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
